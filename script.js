621cf16b2bead6c13145b93e042eaa65406fc20b
Tosin-webdev <oladejit3@gmail.com>
2021-04-07 04:48:26 +0100

    added comment

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, " A", "B", "C", " D", "E", "f"];
 var btn = document.querySelector(".btn");
 var color = document.querySelector(".color");
 
+// functions
 function getRandomNumber() {
   return Math.floor(Math.random() * num.length);
 }
